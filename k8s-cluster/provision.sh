913312b6ed917a90d17fb67451afea569307fe5f
name <you@example.com>
2021-08-21 17:08:50 +0000

    update

--- k8s-cluster/provision.sh
+++ k8s-cluster/provision.sh
@@ -14,11 +14,44 @@ usage() {
     echo " -n, --name  name of stack"
 }
 
+#### To validate the k8s property file
+k8s_prop () 
+{
+if [ ! -f config.properties ]
+then
+echo "config.properties file doesn't Exist, Please create with version tag. Eg: version=1.19 "
+exit 255
+elif [ ! $(grep version config.properties) ]
+then
+echo "'version' property not defined in config.properties file , Please set Eg: version=1.19 "
+exit 255
+elif [ -z $(grep version config.properties|awk -F'=' '{print $2}') ]
+then
+echo "'version' property is null , Please set Eg: version=1.19 "
+exit 255
+fi
+}
+
 #### To validate the Arguments
 if [ $# -eq 2 ]
 then
-    echo "Executing Stack"
+    echo "Executing Script"
+    aws cloudformation list-exports|grep "MyVPCID" >/dev/null 2>&1
+    if [ $? -ne 0 ]
+    then
+        echo ""
+	echo "Network stack doesnt exist, Executing VPC Stack First ..."
+	cd vpc
+        ./provision.sh -n $2-vpc
+	echo ""
+	cd ../ 
+	k8s_prop
+	echo "Executing K8s Stack ..."
+	echo ""
+    fi
     echo ""
+    k8s_prop
+    echo "Executing K8s Stack ..."
 else
     usage
     exit
@@ -26,8 +59,9 @@ fi
 
 #### Varible Initialization
 echo "EC2_STACK_NAME=$2-k8s" >vars.sh
+ver=$(cat config.properties|grep version|awk -F'=' '{print $2}')
 source vars.sh
 set -ex
 
 #### K8s Stack Creation
-aws cloudformation deploy --template-file k8s.yaml --stack-name $EC2_STACK_NAME --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file k8s.yaml --stack-name $EC2_STACK_NAME --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM --parameter-overrides STK=$EC2_STACK_NAME  VER=$ver
