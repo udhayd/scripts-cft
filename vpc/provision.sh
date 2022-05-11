630f2ef0e43a5de36d4209bc7ad87afd075c856a
name <you@example.com>
2022-05-11 13:37:05 +0000

    Addition of vpc.conf

--- vpc/provision.sh
+++ vpc/provision.sh
@@ -17,16 +17,6 @@ usage() {
 #### To validate the Arguments
 if [ $# -eq 2 ]
 then
-    test_var()
-    {
-    [ -z "$1" ] && echo "Value is Empty" && exit 1
-    }
-    read -p "Enter the VPC_CIDR_BLOCK Eg:"10.0.0.0/16": " VPC_CIDR_BLOCK
-    test_var "$VPC_CIDR_BLOCK"
-    read -p "Enter the PUBLIC_SUBNET_CIDR Eg:"10.0.10.0/24,10.0.20.0/24": " PUBLIC_SUBNET_CIDR
-    test_var "$PUBLIC_SUBNET_CIDR"
-    read -p "Enter the PRIVATE_SUBNET_CIDR Eg:"10.0.100.0/24,10.0.200.0/24": " PRIVATE_SUBNET_CIDR
-    test_var "$PRIVATE_SUBNET_CIDR"
     echo  "Executing Stack"
     echo ""
 else
@@ -36,6 +26,7 @@ fi
 
 #### Varible Initialization
 echo "VPC_STACK_NAME=$2-vpc" >vars.sh
+source vpc.conf
 source vars.sh
 set -ex
 
