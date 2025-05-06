70605cd73af4ce1cf255af07eff4d87941bf19d5
udhayd <59496075+udhayd@users.noreply.github.com>
2025-05-07 02:42:32 +0530

    Update cluster_ingress-deploy.sh

--- k8s-selfmanaged/cluster_ingress-deploy.sh
+++ k8s-selfmanaged/cluster_ingress-deploy.sh
@@ -6,3 +6,10 @@ git clone https://github.com/udhayd/traefik
 cd traefik/chart/
 helm dep update
 helm install ingress . -n ingress
+
+lbip=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[PublicIpAddress,Tags[?Key==\`Name\`].Value|[0],LaunchTime,State.Name]' --output text|column -t|grep nginx|awk '{print $1}')
+echo "
+spec:
+  externalIPs:
+    - $lbip" >/tmp/spec.yaml
+kubectl patch svc ingress-traefik --type merge --patch  "$(cat /tmp/spec.yaml)" -n ingress
