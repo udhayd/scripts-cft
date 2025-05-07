acc5aaa1862480b546af7fa6a07dc7d5289eaba2
udhayd <59496075+udhayd@users.noreply.github.com>
2025-05-08 02:10:52 +0530

    Update

--- k8s-selfmanaged/deploy_ingress.sh
+++ k8s-selfmanaged/deploy_ingress.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 ### Script to deploy ingress controller
-kubelctl create ns ingress
+kubectl create ns ingress
 curl -s https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3|bash
 git clone https://github.com/udhayd/traefik
 cd traefik/chart/
 helm dep update
 helm install ingress . -n ingress
 
-lbip=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[PublicIpAddress,Tags[?Key==\`Name\`].Value|[0],LaunchTime,State.Name]' --output text|column -t|grep -i running|grep nginx|awk '{print $1}')
+#lbip=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[PublicIpAddress,Tags[?Key==\`Name\`].Value|[0],LaunchTime,State.Name]' --output text|column -t|grep -i running|grep nginx|awk '{print $1}')
+lbip=$(grep loadbalancer /etc/hosts|awk '{print $1}')
 echo "
 spec:
   externalIPs:
