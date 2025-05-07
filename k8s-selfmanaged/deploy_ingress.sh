018c1249a9a363ab34db9e0ff0aaab8315d319fe
udhayd <59496075+udhayd@users.noreply.github.com>
2025-05-08 02:59:24 +0530

    Update

--- k8s-selfmanaged/deploy_ingress.sh
+++ k8s-selfmanaged/deploy_ingress.sh
@@ -14,3 +14,24 @@ spec:
   externalIPs:
     - $lbip" >/tmp/spec.yaml
 kubectl patch svc ingress-traefik --type merge --patch  "$(cat /tmp/spec.yaml)" -n ingress
+
+#### Update DNS Record
+lbip=$(grep loadbalancer /etc/hosts|awk '{print $1}')
+KEY="LSA2GRkEwF7voysIuaisgwQ11Zc7uXzCaaJDrUXp";
+ZONE_ID="d8c45322d88ecc995996bda2ab7c1553";
+NAME="*.groofy.help";
+SDNS_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?name=$NAME" \
+-H "Authorization: Bearer $KEY" \
+-H "Content-Type: application/json" \
+| python -m json.tool|grep -w "id"|awk -F':' '{print $2}'|sed -e 's/"//g' -e 's/,//g' -e 's/ //g')
+
+DNS_ID="$SDNS_ID";
+TYPE="A";
+NAME="*.groofy.help";
+CONTENT="$lbip";
+PROXIED="false";
+TTL="1";
+curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$DNS_ID" \
+-H "Authorization: Bearer $KEY" \
+-H "Content-Type: application/json" \
+--data '{"type":"'"$TYPE"'","name":"'"$NAME"'","content":"'"$CONTENT"'","proxied":'"$PROXIED"',"ttl":'"$TTL"'}' | python -m json.tool;
