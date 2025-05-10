8615156f03a8482a6667fc0977e28a318ecb76e5
udhayd <59496075+udhayd@users.noreply.github.com>
2025-05-10 23:37:02 +0530

    Update ingress controller script

--- k8s-selfmanaged/deploy_ingress.sh
+++ k8s-selfmanaged/deploy_ingress.sh
@@ -1,22 +1,24 @@
 #! /bin/bash
 ### Script to deploy ingress controller
-kubectl create ns ingress
+export AWS_DEFAULT_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
+
+## Deploy Ingress controller
 curl -s https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3|bash
+kubectl create ns ingress
 git clone https://github.com/udhayd/traefik
 cd traefik/chart/
 helm dep update
 helm install ingress . -n ingress
 
-#lbip=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[PublicIpAddress,Tags[?Key==\`Name\`].Value|[0],LaunchTime,State.Name]' --output text|column -t|grep -i running|grep nginx|awk '{print $1}')
-lbip=$(grep loadbalancer /etc/hosts|awk '{print $1}')
+## Update Ingress service
+lbip=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[PublicIpAddress,Tags[?Key==`Name`].Value|[0],LaunchTime,State.Name]'  --filters Name=instance-state-name,Values=running  --output text|column -t|grep nginx|awk '{print $1}')
 echo "
 spec:
   externalIPs:
     - $lbip" >/tmp/spec.yaml
 kubectl patch svc ingress-traefik --type merge --patch  "$(cat /tmp/spec.yaml)" -n ingress
 
-#### Update DNS Record
-lbip=$(grep loadbalancer /etc/hosts|awk '{print $1}')
+## Update DNS Record
 KEY="LSA2GRkEwF7voysIuaisgwQ11Zc7uXzCaaJDrUXp";
 ZONE_ID="d8c45322d88ecc995996bda2ab7c1553";
 NAME="*.groofy.help";
