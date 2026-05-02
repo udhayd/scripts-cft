0e040b054f6038cbb0e12329c4957c65890c28d1
udhayd <59496075+udhayd@users.noreply.github.com>
2026-05-02 21:15:38 +0530

    update variable

--- k8s-selfmanaged/deploy_ingress.sh
+++ k8s-selfmanaged/deploy_ingress.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 ### Script to deploy ingress controller
-export AWS_DEFAULT_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
+shopt -s expand_aliases
+
+## Variables
+export DOMAIN="groofy.xyz"
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+export AWS_DEFAULT_REGION=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
+alias python=/usr/bin/python3
 
 ## Deploy Ingress controller
 curl -s https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3|bash
