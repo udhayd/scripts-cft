3a0c15c474fd492510be0cf6917d103c35088903
udhayd <59496075+udhayd@users.noreply.github.com>
2026-07-27 00:20:36 +0530

    Update provision.sh

--- okd/provision.sh
+++ okd/provision.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 ##################################################################################################################
-####  Description: Wrapper Script to create a AWS Cloudformation Stack for Local Managed Kubernetes Cluster   ####
+####  Description: Wrapper Script to create a AWS Cloudformation Stack to create OKD Openshift Cluster UPI    ####
 ####  Version: 1.0v                                                                                           ####
 ####  Usage: ./provision.sh -n stackname                                                                      ####
 ##################################################################################################################
@@ -10,7 +10,7 @@
 #### Function for help
 usage() {
     echo "Usage: $0 [options]"
-    
+
     echo " -n, --name  name of stack"
 }
 
@@ -22,16 +22,16 @@ then
     if [ $? -ne 0 ]
     then
         echo ""
-        echo "Network stack doesnt exist, Executing VPC Stack First ..."
+        echo "Network stack doesnt exist, Creating Network stack ..."
         cd ../vpc
         ./provision.sh -n $2
         echo ""
         cd ../okd
-	echo ""
-        echo "Executing K8s Stack ..."
+        echo ""
+        echo "Creating OKD Stack ..."
     fi
     echo ""
-    echo "Executing K8s Stack ..."
+    echo "Creating OKD Stack ..."
 else
     usage
     exit
@@ -39,11 +39,17 @@ fi
 
 #### Varible Initialization
 source config.properties
-echo "EC2_STACK_NAME=$2-k8s" >vars.sh
+CRED1=$AWS_ACCESS_KEY_ID
+CRED2=$AWS_SECRET_ACCESS_KEY
+echo "OKD_CONFIG_STACK_NAME=$2-okd-config" >vars.sh
+echo "OKD_CL_STACK_NAME=$2-okd-cluster" >>vars.sh
 source vars.sh
 set -ex
 
-#### K8s Stack Creation
+#### okd Stack Creation
 AMIID=$(aws ec2 describe-images --owners amazon --filters "Name=name,Values=RHEL-9*x86_64*" --query 'sort_by(Images, &CreationDate)[].ImageId' |tail -2|head -1|sed 's/"//g')
 Image=$(echo -n $AMIID)
-aws cloudformation deploy --template-file k8s.yaml --stack-name $EC2_STACK_NAME --no-fail-on-empty-changeset --capabilities CAPABILITY_NAMED_IAM --parameter-overrides STK=$EC2_STACK_NAME  ImageId=$Image HOSTZONE=$DOMAIN
+
+aws cloudformation deploy --template-file okd-config.yaml --stack-name $OKD_CONFIG_STACK_NAME --no-fail-on-empty-changeset --capabilities CAPABILITY_NAMED_IAM --parameter-overrides STK=$OKD_CONFIG_STACK_NAME CRED1=$CRED1 CRED2=$CRED2 ImageId=$Image HOSTZONE=$DOMAIN
+
+aws cloudformation deploy --template-file okd-cluster.yaml --stack-name $OKD_CL_STACK_NAME --no-fail-on-empty-changeset --capabilities CAPABILITY_NAMED_IAM
